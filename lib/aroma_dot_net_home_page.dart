3e6590573e2dda3236ec6d74b97e1c87e5f88535
yuu-eguci <yuu.eguci@gmail.com>
2021-11-28 21:13:46 +0900

    ✨ bottomNavigationBar で "Home" "Factory" "Purchase" "お知らせ" "Account"

--- lib/aroma_dot_net_home_page.dart
+++ lib/aroma_dot_net_home_page.dart
@@ -22,6 +22,41 @@ class AromaDotNetHomePage extends StatelessWidget {
 
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        // items が4個以上のときは fixed が必要。
+        // 無いとアイコンが白くなる。意味不明だが。
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.filter_hdr),
+            label: 'Factory',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: 'Purchase',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.ring_volume),
+            label: 'お知らせ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance),
+            label: 'Account',
+          ),
+      ],
+      onTap: (int index) {
+        // ここには items の index 番号が入ってくる。
+        if (index == 0) {
+          // "Already a User" は今回何もしない。
+        } else if (index == 1) {
+          Navigator.pushNamed(context, '/sign-up-page');
+        }
+      },
+      )
     );
   }
 }
